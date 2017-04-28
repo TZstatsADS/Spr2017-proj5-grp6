9020a65feff863879be1a3bbd1d9867fa578e5f7
Xiaowo Sun <prayerest@users.noreply.github.com>
2017-04-27 21:55:04 -0400

    Add files via upload

--- app/server.R
+++ app/server.R
@@ -3,11 +3,31 @@ library(shiny)
 library(DT)
 library(plotly)
 library(shinydashboard)
+library(RColorBrewer)
 library(leaflet)
+library(dplyr)
+library(tidyr)
+library(readr)
 
+
+score <- read.csv("../data/scorer2_2015-17.csv")
+# goal <- select(score, Player, Team, Goals, Year)
+# names(goal) <- c("player", "team", "goals", "year")
+# mins <- select(score, Player, Team, Play, Mins, Avg_mins, Year)
+# names(mins) <- c("player", "team", "play", "mins", "avg_mins", "year")
+
+
+### data
 load("../output/FIFAFull.RData")
 load("../output/FIFAPrem.RData")
 load("../output/Player_stats.RData")
+load("../output/Fulldata15.RData")
+load("../output/Fulldata16.RData")
+load("../output/Fulldata.RData")
+load("../output/Prem_player.RData")
+matches <- read.csv("../data/matches.csv")
+
+
 
 server = function(input, output) {
   
@@ -22,95 +42,276 @@ server = function(input, output) {
   
   
   observe({
-    output$barPlot <- renderPlotly({
-      selected <- Prem[(Prem$Pos1==input$Position)|(Prem$Pos2==input$Position)|(Prem$Pos3==input$Position),]
-      # k means cluster
-      set.seed(1)
-      KCluster <- kmeans(selected[, 19:49], 3, nstart = 20)
+      dataInput<-reactive({
+        Fulldata[((Fulldata$Pos1==input$Position)|(Fulldata$Pos2==input$Position)|(Fulldata$Pos3==input$Position))&(Fulldata$Year==input$season),]
+          # selected15 <- Fulldata15[(Fulldata15$Pos1==input$Position)|(Fulldata15$Pos2==input$Position)|(Fulldata15$Pos3==input$Position),]
+          # selected16 <- Fulldata16[(Fulldata16$Pos1==input$Position)|(Fulldata16$Pos2==input$Position)|(Fulldata16$Pos3==input$Position),]
+      })
       
-      # choose the numeric variables to do pca
+      dataInput1<-reactive({
+          set.seed(1)
+          kmeans(dataInput()[, 19:49], 3, nstart = 20)
+      })
+      
+      output$barPlot <- renderPlotly({
+        # k means cluster
+      
+        # choose the numeric variables to do pca
+        
+        if(input$Position=="GK") {
+          dat <- dataInput()[, 50:54]}
+        else{dat <- dataInput()[, 19:49]}
+        
+        pc <- princomp(dat, cor = TRUE, scores=TRUE)
+        
+        #prepare dat1 for 3-dimensional visualization
+        dat1 <- data.frame(cbind(pc$scores[,1:3],dataInput1()$cluster))
+        
+        dat2<-data.frame(cbind(dataInput()[,2],dataInput()[,6]))
+        dat1<-data.frame(cbind(dat1,dat2))
+        
+        #clusplot(dat,KCluster_CAM$cluster,color = T,shade = T)
+        
+        plot_ly(dat1, 
+                x = dat1[,1], 
+                y = dat1[,2], 
+                z = dat1[,3], 
+                text=paste(dat1$X1,"\t",dat1$X2),
+                type = "scatter3d", 
+                mode = "markers", 
+                color=factor(dat1$V4),
+                hoverinfo="text"
+        )
+          
+      })
+      
+      output$table1 <- renderDataTable({
+        #rows of a position
+        load("../output/pos_table.RData")
+        table1<-pos_table[pos_table$Position==input$Position,c(3,4)]
+        datatable(table1, options=list(searching = F,lengthChange=F,paging=T), rownames=F)
+      })
       
       
-      if(input$Position=="GK") {
-        dat <- selected[, 50:54]}
-      else{dat <- selected[, 19:49]}
-      pc <- princomp(dat, cor = TRUE, scores=TRUE)
+      output$table <- renderDataTable({
+
+        dat3<-data.frame(cbind(dataInput(),t=dataInput1()$cluster))
+        table1<-dat3[which(input$tag==dat3$t),c(2,3,6,9:15)]
+        datatable(table1, options=list(searching = T,lengthChange=F,paging=T), rownames=F)
+      })
       
-      #prepare dat1 for 3-dimensional visualization
-      dat1 <- data.frame(cbind(pc$scores[,1:3],KCluster$cluster))
-      dat2<-data.frame(cbind(selected[,2],selected[,6]))
-      dat1<-data.frame(cbind(dat1,dat2))
       
-      #clusplot(dat,KCluster_CAM$cluster,color = T,shade = T)
+      output$selection <- renderPrint({
+        # click to select a row
+        s1 = input$table_rows_selected
+        # s2 <- event_data("plotly_click")
+        # 
+        dat3<-data.frame(cbind(dataInput(),t=dataInput1()$cluster))
+        table1<-dat3[which(input$tag==dat3$t),c(2,3,6,9:15)]
+        if (length(s1)) {
+          cat('These rows were selected:\n\n')
+          cat(table1[s1,1], sep = '\n')
+        }
+         # if(length(s2)){
+         #   dat3[s2[[x]],]
+         # }
+
+      })
       
-      plot_ly(dat1, 
-              x = dat1[,1], 
-              y = dat1[,2], 
-              z = dat1[,3], 
-              text=paste(dat1$X1,"\t",dat1$X2),
-              type = "scatter3d", 
-              mode = "markers", 
-              color=factor(dat1$V4),
-              hoverinfo="text"
-      )
+      output$plotlydef <- renderPlotly({
+        namelist <- dataInput()$Name[dataInput1()$cluster==input$tag]
+        d <- Prem_player[Prem_player$n1name%in%namelist,]
+        # par(mfrow=c(1,2))
+        {
+        p <- plot_ly(
+          d, x = ~as.numeric(d[,input$stats2]), y = ~as.numeric(d[,input$stats1]),
+          showlegend=FALSE,
+          # Hover text:
+          text = ~paste("Name: ",Player, '<br>Team:',Team),
+          color = ~Team
+          )
+         # p2 <- plot_ly(
+         #    d, x = ~Tackles, y = ~Fouled,
+         #    # Hover text:
+         #    text = ~paste("Name: ",Player, '<br>Team:',Team),
+         #    color = ~Team
+         #  )
+          
+        }
+
+      })
       
-    })
-    
-    output$table1 <- renderDataTable({
-      #rows of a position
-      load("../output/pos_table.RData")
-      table1<-pos_table[pos_table$Position==input$Position,c(3,4)]
-      datatable(table1, options=list(searching = F,lengthChange=F,paging=T), rownames=F)
-    })
-    
-    
-    output$table <- renderDataTable({
-      #rows of a position
-      selected <- Prem[(Prem$Pos1==input$Position)|(Prem$Pos2==input$Position)|(Prem$Pos3==input$Position),]
-      # k means cluster
-      set.seed(1)
-      KCluster <- kmeans(selected[, 19:49], 3, nstart = 20)
-      dat3<-data.frame(cbind(selected,t=KCluster$cluster))
-      table1<-dat3[which(input$tag==dat3$t),c(2,3,6,9:15)]
-      datatable(table1, options=list(searching = T,lengthChange=F,paging=T), rownames=F)
-    })
-    
-    
-    output$selection <- renderPrint({
-      # click to select a row
-      s1 = input$table_rows_selected
-      s2 <- event_data("plotly_click")
-      selected <- Prem[(Prem$Pos1==input$Position)|(Prem$Pos2==input$Position)|(Prem$Pos3==input$Position),]
-      # k means cluster
-      set.seed(1)
-      KCluster <- kmeans(selected[, 19:49], 3, nstart = 20)
-      dat3<-data.frame(cbind(selected,t=KCluster$cluster))
-      table1<-dat3[which(input$tag==dat3$t),c(2,3,6,9:15)]
-      if (length(s1)) {
-        cat('These rows were selected:\n\n')
-        cat(table1[s1,1], sep = ', ')
-      }
-      # if(length(s2)){
-      #   dat3[s2[[x]],]
-      # }
+      output$plotlypass <- renderPlotly({  
+        s <- event_data("plotly_click", source = "plotlydef")
+        namelist <- dataInput()$Name[dataInput1()$cluster==input$tag]
+        d <- Prem_player[Prem_player$n1name%in%namelist,]
+        # par(mfrow=c(1,3))
+        p <- plot_ly(
+          d, x = ~as.numeric(d[,input$stats4]), y = ~as.numeric(d[,input$stats3]),
+          showlegend=FALSE,
+          # Hover text:
+          text = ~paste("Name: ",Player, '<br>Team:',Team),
+          color = ~Team
+          ) 
+
+      })
       
-    })
-    
-    # output$scatterplot <- renderPlotly({
-    #   s <- event_data("plotly_click", source = "heatplot")
-    #   if (length(s)) {
-    #     vars <- c(s[["x"]], s[["y"]])
-    #     d <- setNames(mtcars[vars], c("x", "y"))
-    #     yhat <- fitted(lm(y ~ x, data = d))
-    #     plot_ly(d, x = ~x) %>%
-    #       add_markers(y = ~y) %>%
-    #       add_lines(y = ~yhat) %>%
-    #       layout(xaxis = list(title = s[["x"]]), 
-    #              yaxis = list(title = s[["y"]]), 
-    #              showlegend = FALSE)
-    #   } else {
-    #     plotly_empty()
-    #   }
-    # })
+      output$plotlyscore <- renderPlotly({
+        s <- event_data("plotly_click", source = "plotlydef")
+        namelist <- dataInput()$Name[dataInput1()$cluster==input$tag]
+        d <- Prem_player[Prem_player$n1name%in%namelist,]
+
+        p <- plot_ly(
+          d, x = ~as.numeric(d[,input$stats6]), y = ~as.numeric(d[,input$stats5]),
+          #showlegend=FALSE,
+          # Hover text:
+          text = ~paste("Name: ",Player, '<br>Team:',Team),
+          color = ~Team
+        )
+
+      })
+
+
   })
-}
+    
+    
+    ### ashan
+    
+    #year1 <- reactive({
+    #  input$year_player
+    #})
+    
+    # similar
+    # Pcluster <- reactive({
+    # input$cluster
+    #})
+    
+    #Pname <- reactive({
+    #  input$name
+    #})
+    
+    #first chart: playergoal  
+    output$playergoal<-renderPlotly({
+      #temp <- subset(goal, 
+      #               (year == year1())
+                     #add one more cluster filter 
+                     #cluster == Pcluster()
+
+      namelist <- dataInput()$Name[dataInput1()$cluster==input$tag]
+      temp <- Prem_player[Prem_player$n1name%in%namelist,]
+      
+      temp %>%
+        plot_ly(x = ~Team) %>%
+        add_trace(y = ~Goal, type="scatter", marker = list(color = "red", size = 10), text = ~Player,
+                  mode = "markers") %>%
+        layout(
+          title = "Player goals by team",
+          xaxis = list(title = "", tickfont = list(size = 10))
+        )
+        
+      
+    })
+    
+
+    #year1 <- reactive({
+    #  input$year_player
+    #})
+    
+    # similar
+    # Pcluster <- reactive({
+    # input$cluster
+    #})
+    
+    #Pname <- reactive({
+    #  input$name
+    #})
+    
+    #second chart: playertime  
+    output$playertime<-renderPlotly({
+      #temp <- subset(mins, 
+                     #(year == year1())
+                     #add one more cluster filter
+                     #cluster == Pcluster()
+
+      namelist <- dataInput()$Name[dataInput1()$cluster==input$tag]
+      temp <- Prem_player[Prem_player$n1name%in%namelist,]
+
+      
+      colourCount = length(unique(temp$team))
+      getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+      cols = getPalette(colourCount)
+      temp$color <- factor(temp$team, labels = cols)
+      temp %>%
+        plot_ly(x = ~play) %>%
+        add_trace(y = ~avg_mins, type="scatter", marker = list(color = ~color, size = 10), text = ~paste(team, player, sep = ' : '),
+                  mode = "markers") %>%
+        layout(
+          title = "Player average time",
+          xaxis = list(title = "", tickfont = list(size = 10))
+          
+          
+        )
+      
+    })
+    
+    
+    
+    #third chart: polar chart
+    
+    #year1 <- reactive({
+    #  input$year_player
+    #})
+    
+    # similar
+    # Pcluster <- reactive({
+    # input$cluster
+    #})
+    
+    #Pname <- reactive({
+    #  input$name
+    #})
+    
+
+      #namelist <- dataInput()$Name[dataInput1()$cluster==input$tag]
+      #temp <- Prem_player[Prem_player$n1name%in%namelist,]
+
+
+
+    output$spider<-renderPlotly({
+      temp_radar <- select(Prem, Name, #Ball_Control, 
+                           Aggression, Acceleration,
+                           Finishing, Jumping, Heading)
+      #change temp_name when merge
+      temp_name <- subset(temp_radar, (Name %in% Pname()))
+      # temp_show <- select(temp_name,  #Ball_Control, 
+      #                     Aggression, Acceleration,
+      #                     Finishing, Jumping, Heading)
+      tdshow <- gather(temp_name, Ability, Score ,-Name)
+      
+      tdshow %>%
+        plot_ly(r = ~Score, t = ~Ability) %>%
+        add_area(color = ~Name) %>%
+        layout(
+          radialaxis = list(ticksuffix = "%", range = c(0, 100), showticklabels = TRUE),
+          angularaxis = list(showticklabels = TRUE,
+                             tickorientation = 'horizontal', ticklen = 6, visible = TRUE),
+          xaxis = list(
+            showgrid = FALSE, 
+            showline = FALSE, 
+            showticklabels = FALSE, 
+            zeroline = FALSE
+          ),
+          yaxis = list(
+            showgrid = FALSE, 
+            showline = FALSE, 
+            showticklabels = FALSE, 
+            zeroline = FALSE
+          ),
+          showlegend = TRUE,
+          legend = list(orientation = 'h'))
+      
+    })
+    
+    
+    
+  }
--- app/ui.R
+++ app/ui.R
@@ -1,14 +1,31 @@
-## Packages
 library(shinythemes)
 library(shiny)
 library(DT)
 library(plotly)
 library(shinydashboard)
+library(RColorBrewer)
 library(leaflet)
+library(dplyr)
+library(tidyr)
+library(readr)
 
+
+score <- read.csv("../data/scorer2_2015-17.csv")
+goal <- select(score, Player, Team, Goals, Year)
+names(goal) <- c("player", "team", "goals", "year")
+mins <- select(score, Player, Team, Play, Mins, Avg_mins, Year)
+names(mins) <- c("player", "team", "play", "mins", "avg_mins", "year")
+
+
+### data
 load("../output/FIFAFull.RData")
 load("../output/FIFAPrem.RData")
 load("../output/Player_stats.RData")
+load("../output/Fulldata15.RData")
+load("../output/Fulldata16.RData")
+load("../output/Fulldata.RData")
+load("../output/Prem_player.RData")
+matches <- read.csv("../data/matches.csv")
 
 ## UI Function
 
@@ -54,23 +71,30 @@ ui<- navbarPage(
 
                                   column(width = 3,
                                       box(width = NULL, status = "warning",
-                            
+                                          
+                                          selectInput("season",
+                                                      label = "Select the Season", 
+                                                      choices = c("2015","2016"), selected="2016")),
+                                      
                                           selectInput("Position",
                                                       label="Start searching by position", 
-                                                      choices=unique(Prem$Pos1)),  #choose player position
+                                                      choices=unique(Prem$Pos1),
+                                                      selected = "ST"),  #choose player position
                             
-                                          DT::dataTableOutput("table1"),
+                                          
                             
                                           selectInput("tag",
                                                       label=" Select Player by play style", 
                                                       choices=c(1,2,3)),#orig_1617$CUISINE.DESCRIPTION
-                                          verbatimTextOutput("selection")
+                                      
+                                          DT::dataTableOutput("table1")
+                                          #verbatimTextOutput("selection")
                             
                                       )
                         
                                   )
                               )
-                      )
+                      
 
                       
              ),
@@ -84,6 +108,15 @@ ui<- navbarPage(
                       
                                  sidebarLayout(
                                    sidebarPanel(
+
+                                       selectizeInput('year_player', 'Year',choices = goal$year, selected=""),
+                                       selectizeInput('name', 'Player Name',choices = Prem$Name, selected=""),
+   
+                                      #check box of the polar chart
+                                      checkboxGroupInput('name', label = h3("Player Name"), 
+                                                          choices = Prem$Name,
+                                                          selected =""),
+
                                      
                                      width = 3
                                      
@@ -95,24 +128,70 @@ ui<- navbarPage(
                                        
                                        ### 6-dim
                                        tabPanel("scorer, passing, defensive",
-                                                titlePanel("scorer, passing, defensive")
+                                                titlePanel("scorer, passing, defensive"),
+
+                                                fluidRow(
+                                                    column(2,  selectInput(
+                                                          "stats1",label="y axis stats", choices=colnames(Prem_player)[c(3,4:10,15:31)],selected = "Goals.P")  #choose player position
+                                                    ),
+                                                    column(2,  selectInput(
+                                                          "stats2",label="x axis stats", choices=colnames(Prem_player)[c(3,4:10)],selected = "Shots")  #choose player position
+                                                    )),
+                                                    
+                                                fluidRow( 
+                                                    column(9,plotlyOutput("plotlydef")
+                                                    )),
+                                                
+                                                fluidRow(    
+                                                    column(2,  selectInput(
+                                                          "stats3",label="y axis stats", choices=colnames(Prem_player)[c(3,4:10,15:31)],selected = "Tackles")  #choose player position
+                                                    ),
+                                                    column(2,  selectInput(
+                                                          "stats4",label="x axis stats", choices=colnames(Prem_player)[c(15:23)],selected = "Fouls")  #choose player position
+                                                    )),
+                                                
+                                                fluidRow(
+                                                    column(9,plotlyOutput("plotlypass")
+                                                    )),
+                                                
+                                                fluidRow(
+                                                    column(2,  selectInput(
+                                                          "stats5",label="y axis stats", choices=colnames(Prem_player)[c(3,4:10,15:31)],selected = "Pass.Success")  #choose player position
+                                                    ),
+                                                    column(2,  selectInput(
+                                                          "stats6",label="x axis stats", choices=colnames(Prem_player)[c(24:32)],selected = "Passes")  #choose player position
+                                                    )),
+                                                fluidRow(    
+                                                    column(10,offset=0.6,plotlyOutput("plotlyscore")
+                                                    ))
+                                                
+                                                
+                                                #plotlyOutput("plotlydef"),
+                                                #plotlyOutput("plotlypass"),
+                                                #plotlyOutput("plotlyscore")                          
                                      
                                        ),
                                        
                                        ### goal/team
                                        tabPanel("Goal/Team",
-                                                titlePanel("Goal/Team")
+                                                titlePanel("Goal/Team"),
+
+                                                plotlyOutput("playergoal")
                                                 
                                        ),
                                        
                                        ###avg.
-                                       tabPanel("avg.",
-                                                titlePanel("avg.")
+                                       tabPanel("Minutes per Game",
+                                                titlePanel("avg."),
+
+                                                plotlyOutput("playertime")
                                        ),
                                                 
                                        ### radar
                                        tabPanel("radar",
-                                                titlePanel("radar")
+                                                titlePanel("radar"),
+
+                                                plotOutput("spider")
                                        )
                                        
                                    
@@ -125,27 +204,13 @@ ui<- navbarPage(
                                    
                                    position = "right"
                                     ### comparison
-
-
-                
-
-
-
                                 )
                       
                       
-             )
              
              
-             ### Motion Chart
-             #tabPanel("Motion Chart",
-             #         mainPanel(
-             #           
-             #         )
-             #)
-             ### end Motion Chart
-             ## end Player tab
-             
+                       
+  )
   ),
   
   ## Team tab
@@ -159,4 +224,5 @@ ui<- navbarPage(
   
   ## end team tab
 )
+
 
